00d9f4524c72ebcaf0021e7dcf353542598c3cae
Markus Steinberg <markussteinberg@web.de>
2019-02-15 13:07:06 +0100

    RdfTemplateConfig now accessed in the recommended way

--- src/main/java/org/opendatakit/aggregate/server/RdfTemplateConfigManager.java
+++ src/main/java/org/opendatakit/aggregate/server/RdfTemplateConfigManager.java
@@ -3,6 +3,7 @@ package org.opendatakit.aggregate.server;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.opendatakit.aggregate.client.form.RdfExportOptions;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
@@ -11,7 +12,9 @@ import java.io.IOException;
 public class RdfTemplateConfigManager {
     public static RdfExportOptions getRdfExportOptions(){
         try {
-            File file = ResourceUtils.getFile("classpath:rdfExport/rdfExportTemplateConfig.yml");
+            //https://www.baeldung.com/spring-classpath-file-access
+            //Maven's src/main/resources are automatically added to the root of the classpath
+            File file = new ClassPathResource("rdfExport/rdfExportTemplateConfig.yml").getFile();
             ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
             return yamlMapper.readValue(file, RdfExportOptions.class);
         } catch (IOException e) {
