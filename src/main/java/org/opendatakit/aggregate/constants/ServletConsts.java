922b0818e18b96ac8d50d76373d5e2c56ef23ef7
Markus Steinberg <markussteinberg@web.de>
2019-04-12 17:53:40 +0200

    Filetype now set in template config

--- src/main/java/org/opendatakit/aggregate/constants/ServletConsts.java
+++ src/main/java/org/opendatakit/aggregate/constants/ServletConsts.java
@@ -131,7 +131,9 @@ public final class ServletConsts {
 
   public static final String JSON_FILENAME_APPEND = "_results.json";
 
-  public static final String RDF_FILENAME_APPEND = "_results.ttl";
+  public static final String RDF_FILENAME_APPEND = "_results";
+
+  public static final String RDF_FILENAME_TYPE_FALLBACK = ".ttl";
 
   public static final String RECORD_KEY = "record";
 
